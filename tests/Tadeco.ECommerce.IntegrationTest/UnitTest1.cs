c19e5c33e455eb7d7e4447ecc82e5c158229c8cf
anwerhedfi <anwer.hedfi@gmail.com>
2023-04-16 03:33:40 +0100

    set up projects #1

---
+++ tests/Tadeco.ECommerce.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tadeco.ECommerce.IntegrationTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/Tadeco.ECommerce.UnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tadeco.ECommerce.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
